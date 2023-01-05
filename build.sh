c68161459faff9c00365cb7a4290b86a9dc5b27d
JulianRivers <juliancamilorifo@ufps.edu.co>
2023-01-05 18:36:01 -0500

    ready for render.com

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requerimients
+
+python manage.py collectstatic --no-input
+python manage.py makemigrations
+ython manage.py makemigrations projects
+python manage.py migrate
+python manage.py migrate projects
